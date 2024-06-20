c92006ee7d0f3ebb6b984ffcefc2445551e85ac5
Monir Hossain <reactsrapid@gmail.com>
2024-06-20 17:18:44 +0300

    Add files via upload

---
+++ rev.py
@@ -0,0 +1,12 @@
+'''for number in range (10,0,-1):
+    print(number)#reverse'''
+
+
+total = 1
+for number in range(1, 7):
+    total = total * number
+
+print(total)  # This will print 720
+
+
+   
